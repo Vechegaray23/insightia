2008b0df396ebcaafb1183535ea77cbba90f5aaa
Vicente Echegaray <viechegaray@alumnos.uai.cl>
2025-07-19 19:27:24 -0400

    hjk

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,7 @@ set -e
 
 envsubst < /etc/promtail.yml > /tmp/promtail.yml
 promtail -config.file=/tmp/promtail.yml &
-exec uvicorn backend.app.main:app --host 0.0.0.0 --port "${PORT:-8000}" --workers 1
+uvicorn backend.app.main:app \
+  --host 0.0.0.0 \
+  --port "${PORT:-8000}" \
+  --workers 1 2>&1 | tee -a /var/log/uvicorn.log
